8f19be01ad245c2ca1e3ed57d50e79f07bc70429
Mike Clay <mclay692@gmail.com>
2025-09-05 16:11:13 -0600

    Finshed part 3, Number Game

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -2,8 +2,34 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101); // Random number between 1 and 100
+        int guess = 0;
+
+        Console.WriteLine("Welcome to the Magic Number game!");
+        Console.WriteLine("I have chosen a number between 1 and 100. Try to guess it!");
+
+        // Loop until the user guesses correctly
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            string input = Console.ReadLine();
+            guess = int.Parse(input);
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
