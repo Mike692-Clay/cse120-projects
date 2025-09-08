4a19fdcf6853034c8ab681df3bf59a1c7b47d5c3
Mike Clay <mclay692@gmail.com>
2025-09-08 13:32:17 -0600

    Resume for Amanda

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,13 +1,32 @@
 using System;
-public class Job
-{
-    public string _jobTitle;
-    public string _company;
-    public int _startYear;
-    public int _endYear;
 
-    public void Display()
+class Program
+{
+    static void Main(string[] args)
     {
-        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+        // Create first job
+        Job job1 = new Job();
+        job1._jobTitle = "Line Cook";
+        job1._company = "McDonalds";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        // Create second job
+        Job job2 = new Job();
+        job2._jobTitle = "Crew Member";
+        job2._company = "McDonalds LLC";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        // Create resume
+        Resume myResume = new Resume();
+        myResume._personName = "Amanda Jones";
+
+        // Add jobs to resume
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        // Display resume
+        myResume.Display();
     }
 }
---
+++ week02/Resumes/Resume.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+
+public class Resume
+{
+    // Member variables
+    public string _personName;
+    public List<Job> _jobs = new List<Job>();
+
+    // Behavior: Display resume details
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_personName}");
+        Console.WriteLine("Jobs:");
+
+        foreach (Job job in _jobs)
+        {
+            job.Display();
+        }
+    }
+}
